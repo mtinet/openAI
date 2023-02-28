28d66a7d0789f39e6e9328187bdff24e68cccf9d
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-28 14:51:32 +0900

    Update image.py

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import requests
 
 # api키 마지막자 수정해야 함
 openai.api_key = "sk-5FnoGbFja1Gm1jDD4sRnT3BlbkFJ4HSCEGl6fFIglXt6Mv9-"
@@ -15,7 +16,7 @@ image_url = response['data'][0]['url']
 
 # 이미지 가져오기
 image = requests.get(image_url)
-img = Image.open(BytesIO(image.content))
+img = image.open(BytesIO(image.content))
 
 # 이미지 보여주기
 img.show()
